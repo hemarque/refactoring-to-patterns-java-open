3fcbad3b466b4be5cd032d693b07b7f9f84c25f7
Helder De Oliveira <hemarque@gmail.com>
2022-11-14 19:14:22 +0100

    refactor: min price not less than max price

--- src/main/java/team/codium/refactoring_to_patterns/application/AddAlert.java
+++ src/main/java/team/codium/refactoring_to_patterns/application/AddAlert.java
@@ -47,46 +47,45 @@ public class AddAlert {
                         Integer maximumSquareMeters) throws InvalidPostalCodeException, InvalidPriceException, InvalidUserIdException, InvalidAlertTypeException {
         if (Pattern.matches("^\\d{5}$", postalCode)) {
             if (minimumPrice == null || minimumPrice >= 0) {
-                if (minimumPrice == null || maximumPrice == null || minimumPrice <= maximumPrice) {
-                    if (!isAlertTypeValid(alertType)) {
-                        throw new InvalidAlertTypeException("The alert type " + alertType + " does not exist");
-                    }
-                    String usersAsString = readJSONFileContent(usersFile);
-                    User[] users = new Gson().fromJson(usersAsString, User[].class);
-                    boolean userExists = Arrays.stream(users).anyMatch(user -> user.getId() == userId);
-                    if (!userExists) {
-                        throw new InvalidUserIdException("The user " + userId + " does not exist");
-                    }
-                    ArrayList<Alert> alerts = readAlerts();
-                    Alert alert = new Alert(userId, alertType, postalCode, minimumPrice, maximumPrice, minimumRooms, maximumRooms, minimumSquareMeters, maximumSquareMeters);
-                    alerts.add(alert);
-                    try {
-                        Files.writeString(Paths.get(alertsFile), new Gson().toJson(alerts));
-                    } catch (IOException e) {
-                    }
-
-                    if (logger != null) {
-                        HashMap<String, Object> data = new HashMap<>() {{
-                            put("userId", userId);
-                            put("alertType", alertType);
-                            put("postalCode", postalCode);
-                            put("minimumPrice", minimumPrice);
-                            put("maximumPrice", maximumPrice);
-                            put("minimumRooms", minimumRooms);
-                            put("maximumRooms", maximumRooms);
-                            put("minimumSquareMeters", minimumSquareMeters);
-                            put("maximumSquareMeters", maximumSquareMeters);
-                        }};
-                        if (addDateToLogger) {
-                            data.put("date", LocalDate.now());
-                        }
-                        logger.log(data);
-                    }
-
-
-                } else {
+                if (!(minimumPrice == null || maximumPrice == null || minimumPrice <= maximumPrice)) {
                     throw new InvalidPriceException("The minimum price should be bigger than the maximum price");
                 }
+                if (!isAlertTypeValid(alertType)) {
+                    throw new InvalidAlertTypeException("The alert type " + alertType + " does not exist");
+                }
+                String usersAsString = readJSONFileContent(usersFile);
+                User[] users = new Gson().fromJson(usersAsString, User[].class);
+                boolean userExists = Arrays.stream(users).anyMatch(user -> user.getId() == userId);
+                if (!userExists) {
+                    throw new InvalidUserIdException("The user " + userId + " does not exist");
+                }
+                ArrayList<Alert> alerts = readAlerts();
+                Alert alert = new Alert(userId, alertType, postalCode, minimumPrice, maximumPrice, minimumRooms, maximumRooms, minimumSquareMeters, maximumSquareMeters);
+                alerts.add(alert);
+                try {
+                    Files.writeString(Paths.get(alertsFile), new Gson().toJson(alerts));
+                } catch (IOException e) {
+                }
+
+                if (logger != null) {
+                    HashMap<String, Object> data = new HashMap<>() {{
+                        put("userId", userId);
+                        put("alertType", alertType);
+                        put("postalCode", postalCode);
+                        put("minimumPrice", minimumPrice);
+                        put("maximumPrice", maximumPrice);
+                        put("minimumRooms", minimumRooms);
+                        put("maximumRooms", maximumRooms);
+                        put("minimumSquareMeters", minimumSquareMeters);
+                        put("maximumSquareMeters", maximumSquareMeters);
+                    }};
+                    if (addDateToLogger) {
+                        data.put("date", LocalDate.now());
+                    }
+                    logger.log(data);
+                }
+
+
             } else {
                 throw new InvalidPriceException("Price cannot be negative");
             }
